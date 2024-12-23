a591fa9149a08b81352851e5f982fcec49390d1f
skaletta95 <windows.adam0701@gmail.com>
2024-12-23 22:37:47 +0300

    feat: finish 11 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -236,8 +236,21 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  const length = (() => {
+    let count = 0;
+    while (str[count] !== undefined) {
+      count += 1;
+    }
+    return count;
+  })();
+
+  for (let i = 0; i < length; i += 1) {
+    if (str[i] === letter) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
@@ -255,8 +268,21 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numStr = String(num);
+  const length = (() => {
+    let count = 0;
+    while (numStr[count] !== undefined) {
+      count += 1;
+    }
+    return count;
+  })();
+  for (let i = 0; i < length; i += 1) {
+    if (numStr[i] === String(digit)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
@@ -272,8 +298,34 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  const n = (() => {
+    let count = 0;
+    while (arr[count] !== undefined) {
+      count += 1;
+    }
+    return count;
+  })();
+
+  if (n < 3) return -1;
+
+  let totalSum = 0;
+  for (let i = 0; i < n; i += 1) {
+    totalSum += arr[i];
+  }
+
+  let leftSum = 0;
+  for (let i = 0; i < n; i += 1) {
+    const rightSum = totalSum - leftSum - arr[i];
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+
+    leftSum += arr[i];
+  }
+
+  return -1;
 }
 
 /**
